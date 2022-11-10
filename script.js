bf193cdfab86ef64c79efd7824e9d4c494853a7a
Miguel Urena <urena_miguel82@outlook.com>
2022-11-10 14:33:38 -0800

    Update script.js function

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ setInterval(setClock, 1000)
 function setClock() {
     const currentDate = new Date()
     const secondsRation = currentDate.getSeconds() / 60
-    const minutesRation = currentDate.getMinutes() / 60
-    const hoursRation = currentDate.getHours() / 60
+    const minutesRation = (secondsRation + currentDate.getMinutes()) / 60
+    const hoursRation = (minutesRation + currentDate.getHours()) / 12
 }
